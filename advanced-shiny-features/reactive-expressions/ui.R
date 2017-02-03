45e950387d3150f8d0c6e40dd3db6703c3b8920c
martinjhnhadley <martinjohnhadley@gmail.com>
2017-02-03 11:51:27 +0000

    Initial steps with reactive template

---
+++ advanced-shiny-features/reactive-expressions/ui.R
@@ -0,0 +1,48 @@
+## =============================== License ========================================
+## ================================================================================
+## This work is distributed under the MIT license, included in the parent directory
+## Copyright Owner: University of Oxford
+## Date of Authorship: 2016
+## Author: Martin John Hadley (orcid.org/0000-0002-3039-6849)
+## ================================================================================
+
+library(shinyjs)
+
+shinyUI(
+  fluidPage(
+    useShinyjs(),
+    includeCSS("www/animate.min.css"),
+    # provides pulsating effect
+    includeCSS("www/loading-content.css"),
+    # provides styles for the "loading sheet"
+    wellPanel(
+      p("Below a ggplot2 chart will be shown after a pause of 5 seconds."),
+      p("You can force the loading screen to show again by clicking the submit button."),
+      p("Note that ALL content below where 'loading pane' is greyed out i.e. the loading pane is on top 
+      of all other content."),
+      actionButton("reload", label = "Reload")
+    ),
+    sliderInput("mean", "Mean of rnom(1000, mean, sd)", min = 0, 5),
+    textOutput("plot_label", "Label of plot"),
+    
+    fluidRow(
+      column(
+        plotOutput("histogram_renderPlot"),
+        width = 6
+      ),
+      column(
+        plotOutput("histogram_eventReactive"),
+        
+      )
+    )
+    
+    
+    
+    div(id = "loading-content",
+        class = "loading-content",
+        h2(class = "animated infinite pulse", "Loading data...")),
+    plotOutput("chart", height = "200px"),
+    "Text below the chart, probably explaining why the chart looks the way it does. Note that the \"loading pane\" covers this content too, it is designed to cover the entirety of the screen below where it is called in the ui.R file"
+  )
+  
+)
