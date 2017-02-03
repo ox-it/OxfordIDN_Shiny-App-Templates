fd6fda596209bcb3523968f948570ad85e580718
martinjhnhadley <martinjohnhadley@gmail.com>
2017-02-03 11:48:59 +0000

    Added loading-css template

--- advanced-shiny-features/loading-data/ui.R
+++ advanced-shiny-features/loading-data/ui.R
@@ -19,15 +19,14 @@ shinyUI(
       p("Below a ggplot2 chart will be shown after a pause of 5 seconds."),
       p("You can force the loading screen to show again by clicking the submit button."),
       p("Note that ALL content below where 'loading pane' is greyed out i.e. the loading pane is on top 
-      of all other content.")
+      of all other content."),
+      actionButton("reload", label = "Reload")
     ),
-    actionButton("reload", label = "Reload"),
     div(id = "loading-content",
-        fillPage(
-          h2(class = "animated infinite pulse", "Loading data...")
-        )),
-    plotOutput("chart"),
-    "Text below the chart, probably explaining why the chart looks the way it does."
+        class = "loading-content",
+        h2(class = "animated infinite pulse", "Loading data...")),
+    plotOutput("chart", height = "200px"),
+    "Text below the chart, probably explaining why the chart looks the way it does. Note that the \"loading pane\" covers this content too, it is designed to cover the entirety of the screen below where it is called in the ui.R file"
   )
   
 )
